c859baa8f62aef32dc607c58c65ecdcd508416f5
brandtnorwood <brandtnorwood@gmail.com>
2025-04-02 21:57:15 -0500

    Created readPost handler, basic error handling

--- Backend/main.js
+++ Backend/main.js
@@ -16,6 +16,7 @@ const app = express();
 const bygone = require ('./bygoneBackend');
 const postgres = require('postgres');
 const path = require('path');
+const { request } = require('http');
 
 const sql = postgres(databaseLocation)
 
@@ -32,24 +33,56 @@ app.use(express.json());
 
 
 
-//test query function
-async function getAllData(){
+// POSTGRES FUNCTIONS
+
+
+//returns last x number of posts for main page
+async function getNewPosts(limit){
     const data = await sql`
         SELECT p.id, p.username, p.topic, p.time, p.body, p.likes, p.dislikes, p.catagory, u.commonname
         FROM public.posts p
         JOIN public.users u ON p.username = u.username
         ORDER BY p.id DESC
-        LIMIT 20;`
+        LIMIT ${limit};`
 
     return data
 }
 
 
 
+// returns full data about a post
+async function getPostDetails(postid){
+    const data = await sql`
+        SELECT p.*, u.commonname
+        FROM public.posts p
+        JOIN public.users u ON p.username = u.username
+        WHERE p.id = ${postid};`
+
+    if (data.length != 0) {
+        return data;
+    } else {
+        throw 404;
+    }
+}
+
+
+//  REST HANDLERS
+
+
+
 //Returns last x number of posts
 app.get("/api/newPosts", (req, res) => {
-    getAllData().then((sqlReturn) =>{
-        res.send(sqlReturn)
+    getNewPosts(20)                          //Change this line to change default number of posts
+    .then((queryReturn) =>{
+        res.send(queryReturn)
+    })
+    .catch((error) => {
+        if (error == 404){
+            res.status(404).send("That Post Was Not Found");
+        } else {
+            res.status(500).send("The Rats Messed Up That One!");
+            bygone.output(`ERROR! /api/newPost triggered 500 response due to ${error}`)
+        }
     });
 });
 
@@ -57,7 +90,24 @@ app.get("/api/newPosts", (req, res) => {
 
 //Returns the post and comments of a specific post
 app.get("/api/readPost", (req,res) => {
-    //this is something
+    //make sure the request is being made with data and specificly a number in the postid feild
+    if (!req.body || !req.body.postid ||typeof req.body.postid != "number") {
+        res.status(400).send("The Rats Didn't Understand What Was Requested!");
+        return;
+    }
+
+    getPostDetails(req.body.postid)
+    .then((queryReturn) =>{
+        res.send(queryReturn)
+    })
+    .catch((error) => {
+        if (error == 404){
+            res.status(404).send("That Post Was Not Found");
+        } else {
+            res.status(500).send("The Rats Messed Up That One!");
+            bygone.output(`ERROR! /api/newPost triggered 500 response due to ${error}`)
+        }
+    });
 });
 
 
